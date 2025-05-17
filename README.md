970ca2048c20f83ed1717aa642ed4822a6c44938
Elena Ignatenko <Walden-ea@yandex.ru>
2025-05-17 20:17:43 +0200

    masking tests

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # ADND_Assignment
 
+## Testing
+
+```
+pytest
+```
+
 TODO:
  - add masking DONE
  - transformer + tests DONE
@@ -7,6 +13,6 @@ TODO:
  - cleanup py files DONE
  - decoder test DONE
  - pos en tests DONE
- - add masking tests
- - fix pos encoding
+ - add masking tests DONE
+ - fix pos encoding DONE
  - check everything
--- test/test_transformer.py
+++ test/test_transformer.py
@@ -3,9 +3,10 @@ import sys
 import os
 
 # Add the src directory to the path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 from transformer import Transformer
+
 
 def test_transformer():
     vocab_size = 32
@@ -17,8 +18,46 @@ def test_transformer():
     tokens_encoding = torch.randint(0, vocab_size, (batch_size, seq_len))
     tokens_decoding = torch.randint(0, vocab_size, (batch_size, seq_len))
 
-    output = transf(tokens_encoding, tokens_decoding) 
+    output = transf(tokens_encoding, tokens_decoding)
     # return logits for each token position in the sequence, across all vocabulary entries
 
-    assert output.shape == (batch_size, seq_len, vocab_size), \
-        f"Expected shape {(batch_size, seq_len, vocab_size)}, got {output.shape}"
+    assert output.shape == (
+        batch_size,
+        seq_len,
+        vocab_size,
+    ), f"Expected shape {(batch_size, seq_len, vocab_size)}, got {output.shape}"
+
+
+def test_pad_mask():
+    tf = Transformer()
+
+    token_ids = torch.tensor([1, 2, 3, 4, 0, 0, 0, 0])
+    ref_mask = torch.tensor(
+        (
+            [
+                [[False]],
+                [[False]],
+                [[False]],
+                [[False]],
+                [[True]],
+                [[True]],
+                [[True]],
+                [[True]],
+            ]
+        )
+    )
+    assert torch.equal(tf.create_padding_mask(token_ids), ref_mask)
+
+
+def test_decoder_mask():
+    seq_len = 3
+
+    tf = Transformer()
+    ref_mask = torch.tensor(
+        [
+            [False, True, True], 
+            [False, False, True], 
+            [False, False, False]
+         ]
+    )
+    assert torch.equal(tf.create_look_ahead_mask(3), ref_mask)
