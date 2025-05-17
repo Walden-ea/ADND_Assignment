2017efe403db24d81472c1a5a987f6ba29feaa0d
Lina <galina2002grigoreva@gmail.com>
2025-05-17 14:47:10 +0200

    Add Transformer file

--- src/encoder.py
+++ src/encoder.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 from attention import MultiHeadAttention
 from feed_forward import FFN
-from embedding import Embedding
-from pos_en import PosEncoding
 
 class EncoderLayer(nn.Module):
     def __init__(self, d_model, num_heads, d_k, d_v, batch_size, d_inner, seq_len):
@@ -42,25 +40,3 @@ class Encoder(torch.nn.Module):
         out = self.encoder_layer_5(out)
         out = self.encoder_layer_6(out)
         return out
-
-d_model = 512
-num_heads = 8
-d_k = d_v = 64
-batch_size = 4
-d_inner = 2048
-max_len = 30
-
-Embed = Embedding(max_len, d_model)
-tokens = torch.randint(0, 32, (batch_size, max_len))  # give 32 random token indinces (batch of 4, sequence length 30)
-embedding = Embed(tokens)
-
-PosEnc = PosEncoding(d_model, max_len)
-input = PosEnc(embedding)
-
-print(embedding.shape)
-
-encoder = Encoder(d_model, num_heads, d_k, d_v, batch_size, d_inner, max_len)
-
-output = encoder(input)
-
-print(output.shape) # should be (4, 30, 512)
