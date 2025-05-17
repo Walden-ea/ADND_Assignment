f670f10634c2fd5a69349dacc2f72f568e4897a3
Elena Ignatenko <Walden-ea@yandex.ru>
2025-05-17 16:31:22 +0200

    decoder dump

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ADND_Assignment
+
+TODO:
+ - add masking
+ - transformer + tests
+ - decoder, pos en tests
+ - comms
+ - cleanup py files
+ - check everything
+ - inference check how should work
--- src/decoder.py
+++ src/decoder.py
@@ -1,32 +1,20 @@
 import torch
 from attention import MultiHeadAttention
 from feed_forward import FFN
-
-# class MultiHeadAttention(nn.Module):
-#     # In paper d_model / d = d_k = d_v
-#     def __init__(self, d_model, num_heads, d_k, d_v, batch_size, seq_len):
-
 class DecoderLayer(torch.nn.Module):
     def __init__(self, embed_dim, n_heads, d_k, d_v, batch_size, seq_len, d_inner):
         super(DecoderLayer, self).__init__()
         
         self.self_attn = MultiHeadAttention(embed_dim, n_heads, d_k, d_v, batch_size, seq_len)
-        # Encoder(embed_dim, num_heads, d_k, d_v, batch_size, d_inner, max_len) # TODO: remove dummy
         self.cross_attn = MultiHeadAttention(embed_dim, n_heads, d_k, d_v, batch_size, seq_len)
-        # torch.nn.MultiheadAttention(embed_dim, n_heads) # TODO: remove dummy
 
-        # self.ff = torch.nn.Sequential( # TODO: remove dummy
-        #     torch.nn.Linear(embed_dim, 8),
-        #     torch.nn.ReLU(),
-        #     torch.nn.Linear(8, embed_dim)
-        # )
 
         self.ff = FFN(embed_dim, d_inner)
         self.norm1 = torch.nn.LayerNorm(embed_dim)
         self.norm2 = torch.nn.LayerNorm(embed_dim)
         self.norm3 = torch.nn.LayerNorm(embed_dim)
 
-    def forward(self, x, encoder_data): # TODO: add masking!! ACTUALLY NOT NEEDED
+    def forward(self, x, encoder_data): # TODO: add masking!!
         self_attn_output = self.self_attn(x, x, x)
         x = x + self_attn_output
         x = self.norm1(x)
