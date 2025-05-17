test fixes for masking

src/decoder.py
@@ -36,7 +36,7 @@ class Decoder(torch.nn.Module):
         self.decoder_layer_2 = DecoderLayer(embed_dim, n_heads, d_k, d_v, batch_size, seq_len, d_inner)
         self.decoder_layer_3  = DecoderLayer(embed_dim, n_heads, d_k, d_v, batch_size, seq_len, d_inner)
 
-    def forward(self, x, encoder_data, decoder_mask, encoder_mask):
+    def forward(self, x, encoder_data, decoder_mask = None, encoder_mask = None):
         x = self.decoder_layer_1(x, encoder_data, decoder_mask, encoder_mask)
         x = self.decoder_layer_2(x, encoder_data, decoder_mask, encoder_mask)
         x = self.decoder_layer_3(x, encoder_data, decoder_mask, encoder_mask)

src/encoder.py
@@ -32,7 +32,7 @@ class Encoder(torch.nn.Module):
         self.encoder_layer_5 = EncoderLayer(d_model, num_heads, d_k, d_v, batch_size, d_inner, seq_len)
         self.encoder_layer_6 = EncoderLayer(d_model, num_heads, d_k, d_v, batch_size, d_inner, seq_len)
 
-    def forward(self, x, mask):
+    def forward(self, x, mask = None):
         out = self.encoder_layer_1(x, mask)
         out = self.encoder_layer_2(out, mask)
         out = self.encoder_layer_3(out, mask)

src/transformer.py
@@ -16,7 +16,7 @@ class Transformer(nn.Module):
         self.batch_size = batch_size
         self.d_inner = d_inner
         self.max_len = max_len
-        self.Embedding = Embedding(vocab_size, d_model)
+        self.Embedding = nn.Embedding(vocab_size, d_model)
         self.PosEnc = PosEncoding(d_model, max_len)
         self.Encoder = Encoder(d_model, num_heads, d_k, d_v, batch_size, d_inner, max_len)
         self.Decoder = Decoder(d_model, num_heads, d_k, d_v, batch_size, max_len, d_inner)

test/test_attention.py
@@ -19,7 +19,7 @@ def test_attention():
     K = torch.randn(batch_size, seq_len, d_model)
     V = torch.randn(batch_size, seq_len, d_model)
 
-    mha = MultiHeadAttention(d_model, num_heads, d_k, d_v, batch_size)
+    mha = MultiHeadAttention(d_model, num_heads, d_k, d_v, batch_size, seq_len)
     output = mha(Q, K, V)
 
     assert output.shape == (batch_size, seq_len, d_model), \